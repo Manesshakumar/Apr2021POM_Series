d048d9a0c6c3a881994a782900a24459aaf198cf
Manesshakumar <manishaakumar95@gmail.com>
2021-04-09 10:59:56 +0530

    fixed untracked files

---
+++ src/main/java/com/qa/Utils/Constants.java
@@ -0,0 +1,10 @@
+package com.qa.Utils;
+
+public class Constants {
+	
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
+	public static final String ACCOUNT_SUCCESS_MSG = "Your Account Has Been Created!";
+	public static final String SHEET_NAME = "Registration";
+	
+
+}
---
+++ src/main/java/com/qa/democart/Factory/OptionManager.java
@@ -0,0 +1,43 @@
+package com.qa.democart.Factory;
+
+import java.util.Properties;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class OptionManager {
+	
+	private Properties prop;
+	private ChromeOptions co;
+	private FirefoxOptions fo;
+	
+	
+	public OptionManager(Properties prop) {
+		this.prop = prop;
+	}
+	public ChromeOptions getChromeOptions() {
+		co = new ChromeOptions();
+		if(Boolean.parseBoolean(prop.getProperty("headless").trim())) {
+			co.addArguments("--headless");
+		}
+		if(Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
+			co.addArguments("--incognito");
+		}
+		return co;
+		
+	}
+	
+	public FirefoxOptions getFireFoxOptions() {
+		fo = new FirefoxOptions();
+		if(Boolean.parseBoolean(prop.getProperty("headless").trim())) {
+			fo.addArguments("--headless");
+		}
+		if(Boolean.parseBoolean(prop.getProperty("incognito").trim())) {
+			fo.addArguments("--incognito");
+		}
+		return fo;
+		
+	}
+	
+
+}
